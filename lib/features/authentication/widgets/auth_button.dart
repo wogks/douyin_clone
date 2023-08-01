b5d6cc99d16eaa26ab8bbfb70d86898dc948bfb9
wogks <wogks27@naver.com>
2023-08-01 21:29:24 +0900

    Feat: AuthButton

--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -1,28 +1,39 @@
 import 'package:douyin_clone/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AuthButton extends StatelessWidget {
   final String text;
-  const AuthButton({super.key, required this.text});
+  final FaIcon icon;
+  const AuthButton({super.key, required this.text, required this.icon});
 
   @override
   Widget build(BuildContext context) {
     return FractionallySizedBox(
       widthFactor: 1,
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: Sizes.size14),
+        padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size14, horizontal: Sizes.size14),
         decoration: BoxDecoration(
             border: Border.all(
           color: Colors.grey.shade300,
           width: Sizes.size2,
         )),
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            fontSize: Sizes.size16,
-            fontWeight: FontWeight.w600,
-          ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(alignment: Alignment.centerLeft, child: icon),
+            Expanded(
+              child: Text(
+                text,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
