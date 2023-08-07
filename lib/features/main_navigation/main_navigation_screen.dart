ae60894f9edfb9d463217a46fa6ddcb19f61acd0
wogks <wogks27@naver.com>
2023-08-07 23:24:19 +0900

    Feat: custom navigation bar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,3 +1,5 @@
+import 'package:douyin_clone/constants/sizes.dart';
+import 'package:douyin_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -24,9 +26,6 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     const Center(
       child: Text('search'),
     ),
-    const Center(
-      child: Text('home'),
-    ),
   ];
 
   void _onTap(int index) {
@@ -38,48 +37,40 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _onTap,
-        currentIndex: _selectedIndex,
-        selectedItemColor: Theme.of(context).primaryColor,
-        items: const [
-          BottomNavigationBarItem(
-            label: 'Home',
-            tooltip: 'what are you',
-            icon: FaIcon(
-              FontAwesomeIcons.house,
-            ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: 'Home',
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: 'Discover',
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: 'Inbox',
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: 'User',
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                onTap: () => _onTap(4),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            tooltip: 'what are you',
-            label: 'Search',
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-            ),
-          ),
-          BottomNavigationBarItem(
-            tooltip: 'what are you',
-            label: 'Search',
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-            ),
-          ),
-          BottomNavigationBarItem(
-            tooltip: 'what are you',
-            label: 'Search',
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-            ),
-          ),
-          BottomNavigationBarItem(
-            tooltip: 'what are you',
-            label: 'Search',
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
