fe9e2ae4a2674410fd01992f2663e861152a3ecb
wogks <wogks27@naver.com>
2023-08-07 23:04:34 +0900

    Feat: bottomnavigationbar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text('home'),
+    ),
+    const Center(
+      child: Text('search'),
+    ),
+    const Center(
+      child: Text('home'),
+    ),
+    const Center(
+      child: Text('search'),
+    ),
+    const Center(
+      child: Text('home'),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTap,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            tooltip: 'what are you',
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+            ),
+          ),
+          BottomNavigationBarItem(
+            tooltip: 'what are you',
+            label: 'Search',
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+          ),
+          BottomNavigationBarItem(
+            tooltip: 'what are you',
+            label: 'Search',
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+          ),
+          BottomNavigationBarItem(
+            tooltip: 'what are you',
+            label: 'Search',
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+          ),
+          BottomNavigationBarItem(
+            tooltip: 'what are you',
+            label: 'Search',
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
