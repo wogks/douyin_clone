66183fdb27a5b3bf1b2364f100b1216d311aef48
wogks <wogks27@naver.com>
2023-08-10 22:57:53 +0900

    Feat: grid item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,5 +1,7 @@
+import 'package:douyin_clone/constants/gaps.dart';
 import 'package:douyin_clone/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
@@ -30,23 +32,82 @@ class DiscoverScreen extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(children: [
-          GridView.builder(
-            padding: const EdgeInsets.all(Sizes.size6),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: Sizes.size10,
-              mainAxisSpacing: 10,
-              childAspectRatio: 16/9,
-              
+        body: TabBarView(
+          children: [
+            GridView.builder(
+              padding: const EdgeInsets.all(Sizes.size6),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: Sizes.size10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 9 / 20,
+              ),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      fit: BoxFit.cover,
+                      placeholder:
+                          '/Users/wogks/Desktop/flutterPT/douyin_clone/assets/image/ee.jpg',
+                      image:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8dkWGo6GZS-vbF55vuOQV4SpwAbjFDKz3ig&usqp=CAU',
+                    ),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    ' asddj asddjasddj asddjasddjasddjasddjasddjasddjasddjasddj',
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Gaps.v8,
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 12,
+                        backgroundImage: NetworkImage(
+                            'https://kormedi.com/wp-content/uploads/2022/04/ck_tica1010005154_l-580x387.jpg'),
+                      ),
+                      Gaps.h4,
+                      Expanded(
+                        child: Text(
+                          '저는 자신이 있습니다. 어떤 기능이든지 전부 만들 수 있습니다.',
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                      Gaps.h4,
+                      const FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size14,
+                      ),
+                      Gaps.h2,
+                      Text(
+                        '3M',
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            itemBuilder: (context, index) => ,
-          ),
-          for (var tab in tabs.skip(1))
-            Center(
-              child: Text(tab),
-            ),
-        ]),
+            for (var tab in tabs.skip(1))
+              Center(
+                child: Text(tab),
+              ),
+          ],
+        ),
       ),
     );
   }
