07a5fca16dae327ce71efbb8b46c8ef4e57781ad
wogks <wogks27@naver.com>
2023-08-12 20:00:23 +0900

    Feat: animated part two

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,4 +1,5 @@
 import 'package:douyin_clone/constants/sizes.dart';
+import 'package:douyin_clone/features/inbox/chat_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -19,6 +20,63 @@ class _ChatsScreenState extends State<ChatsScreen> {
       _key.currentState!.insertItem(_items.length);
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: _makeListTile(
+            index,
+          ),
+        ),
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+
+  Widget _makeListTile(int index) {
+    return ListTile(
+      onTap: _onChatTap,
+      onLongPress: () => _deleteItem(index),
+      leading: const CircleAvatar(
+        foregroundImage: NetworkImage(
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN02Q-MzBFJMHx8Gq903bLOMVOIDDXojcYgQ&usqp=CAU'),
+        radius: 30,
+        child: Text('아들'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            '어머이 ($index)',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '2:13 Am',
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text("빨리 취업하거라 아들"),
+    );
   }
 
   @override
@@ -42,36 +100,10 @@ class _ChatsScreenState extends State<ChatsScreen> {
             key: UniqueKey(),
             opacity: animation,
             child: SizeTransition(
-              sizeFactor: animation,
-              child: ListTile(
-                leading: const CircleAvatar(
-                  foregroundImage: NetworkImage(
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN02Q-MzBFJMHx8Gq903bLOMVOIDDXojcYgQ&usqp=CAU'),
-                  radius: 30,
-                  child: Text('아들'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      '어머이 ($index)',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '2:13 Am',
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: const Text("빨리 취업하거라 아들"),
-              ),
-            ),
+                sizeFactor: animation,
+                child: _makeListTile(
+                  index,
+                )),
           );
         },
       ),
