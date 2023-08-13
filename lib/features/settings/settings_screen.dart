4aed6e2d0f71f63368f7143ff95d13f2d01bdf97
wogks <wogks27@naver.com>
2023-08-13 21:51:54 +0900

    Feat: swithlisttile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +25,30 @@ class SettingsScreen extends StatelessWidget {
           title: const Text('Settings'),
         ),
         body: ListView(
-          children: const [
-            AboutListTile(
+          children: [
+            Switch.adaptive(
+              value: _notifications,
+              onChanged: _onNotificationsChanged,
+            ),
+            CupertinoSwitch(
+              value: _notifications,
+              onChanged: _onNotificationsChanged,
+            ),
+            Switch(
+              value: _notifications,
+              onChanged: _onNotificationsChanged,
+            ),
+            Checkbox(
+              value: _notifications,
+              onChanged: _onNotificationsChanged,
+            ),
+            CheckboxListTile(
+              activeColor: Colors.black,
+              value: _notifications,
+              onChanged: _onNotificationsChanged,
+              title: const Text('Enable notifications'),
+            ),
+            const AboutListTile(
               applicationVersion: '1.1',
               applicationLegalese: 'All rights reserved.',
             ),
