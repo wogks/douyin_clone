7458d0af04c74dcfe59b45274c5b239d6d00a897
wogks <wogks27@naver.com>
2023-08-16 21:04:08 +0900

    Feat: camera controller

---
+++ lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,77 @@
+import 'package:camera/camera.dart';
+import 'package:douyin_clone/constants/gaps.dart';
+import 'package:douyin_clone/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+  bool hasPermission = false;
+  late final CameraController _cameraController;
+
+  void initPermission() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      hasPermission = true;
+      await initCamera();
+      setState(() {});
+    } else {}
+  }
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) return;
+    _cameraController =
+        CameraController(cameras[0], ResolutionPreset.ultraHigh);
+    await _cameraController.initialize();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initPermission();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !hasPermission || !_cameraController.value.isInitialized
+            ? const Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "권한을 얻는중...",
+                    style:
+                        TextStyle(color: Colors.white, fontSize: Sizes.size20),
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator(),
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(_cameraController),
+                ],
+              ),
+      ),
+    );
+  }
+}
