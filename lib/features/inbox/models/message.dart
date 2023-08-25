307d8c358a9e2901829a6f4de94c4d4c9eb84af9
wogks <wogks27@naver.com>
2023-08-25 10:44:35 +0900

    Feat: streamprovider

--- lib/features/inbox/models/message.dart
+++ lib/features/inbox/models/message.dart
@@ -1,20 +1,21 @@
 class MessageModel {
   final String text;
   final String userId;
+  final int ceatedAt;
 
-  MessageModel({
-    required this.text,
-    required this.userId,
-  });
+  MessageModel(
+      {required this.text, required this.userId, required this.ceatedAt});
 
   MessageModel.fromJson(Map<String, dynamic> json)
       : text = json['text'],
+        ceatedAt = json['ceatedAt'],
         userId = json['userId'];
 
   Map<String, dynamic> toJson() {
     return {
       'text': text,
       'userId': userId,
+      'ceatedAt': ceatedAt,
     };
   }
 }
--- lib/features/inbox/view_models/messages_view_model.dart
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:douyin_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:douyin_clone/features/inbox/models/message.dart';
 import 'package:douyin_clone/features/inbox/repos/messages_repo.dart';
@@ -16,7 +17,11 @@ class MessagesViewModel extends AsyncNotifier<void> {
     final user = ref.read(authRepo).user;
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      final message = MessageModel(text: text, userId: user!.uid);
+      final message = MessageModel(
+        text: text,
+        userId: user!.uid,
+        ceatedAt: DateTime.now().millisecondsSinceEpoch,
+      );
       _repo.sendMessage(message);
     });
   }
@@ -25,3 +30,17 @@ class MessagesViewModel extends AsyncNotifier<void> {
 final messagesProvider = AsyncNotifierProvider<MessagesViewModel, void>(
   () => MessagesViewModel(),
 );
+
+final chatProvder = StreamProvider<List<MessageModel>>((ref) {
+  final db = FirebaseFirestore.instance;
+  return db
+      .collection('chat_rooms')
+      .doc('xfeHNQkU1Pl3O7tZZjHy')
+      .collection('texts')
+      .orderBy('ceatedAt')
+      .snapshots()
+      .map(
+        (event) =>
+            event.docs.map((doc) => MessageModel.fromJson(doc.data())).toList(),
+      );
+});
